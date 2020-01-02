108e9536c6fbb6f78c45f60d5662a0a51f8fa5c5
bjerkeweb <bjerkeweb@gmail.com>
2020-01-02 16:31:54 -0500

    fix output format

--- src/leaper.js
+++ src/leaper.js
@@ -1,2 +1,32 @@
+let rAF = window.requestAnimationFrame;
+
 const leaper = () => {
+
+  let element;
+
+  let start;
+  let stop;
+
+  let duration;
+
+  let easing;
+
+  let elapsed;
+
+  function getLocation() {
+    return window.scrollY || window.pageYOffset;
+  }
+
+  // public api
+  function api({ target, opts = {} }) {
+    duration = opts.duration || 1000;
+  }
+
+
+  return api;
+
 }
+
+let singleton = leaper();
+
+export default singleton;
